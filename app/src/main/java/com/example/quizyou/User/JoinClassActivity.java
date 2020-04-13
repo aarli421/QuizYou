99f89ff09ed9f6251e6dbb6e75a02b1145df791c
5up3rN00b <aaron.linear@gmail.com>
2020-04-13 16:12:50 -0700

    Finished Make Test Activity

---
+++ app/src/main/java/com/example/quizyou/User/JoinClassActivity.java
@@ -0,0 +1,16 @@
+package com.example.quizyou.User;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizyou.R;
+
+public class JoinClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_class);
+    }
+}
