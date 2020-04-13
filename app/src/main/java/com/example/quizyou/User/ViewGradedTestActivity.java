044993a73e58708c18c03733b8b81567e18f6693
larryz1230 <test@SZHI-US-LE01.am.trimblecorp.net>
2020-04-13 11:36:24 -0700

    Added buttons and scrolls on new activities

--- app/src/main/java/com/example/quizyou/User/ViewGradedTestActivity.java
+++ app/src/main/java/com/example/quizyou/User/ViewGradedTestActivity.java
@@ -1,16 +1,37 @@
 package com.example.quizyou.User;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.quizyou.MainActivity;
 import com.example.quizyou.R;
 
+//TODO implement test objects in array
+
 public class ViewGradedTestActivity extends AppCompatActivity {
+
+    private Button mBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_graded_test);
+
+        mBack = findViewById (R.id.back_button);
+
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StudentActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
