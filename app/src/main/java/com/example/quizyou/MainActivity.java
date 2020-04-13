0fab71f6ee2b857b4da406b9165b23f4ba3233b2
Zheng Andrew <andrewzheng04@gmail.com>
2020-04-12 18:34:57 -0700

    temp

--- app/src/main/java/com/example/quizyou/MainActivity.java
+++ app/src/main/java/com/example/quizyou/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.quizyou;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.quizyou.User.ViewGradedTestActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mgradedTest;
     // TODO Dropdown select if you are a student or a teacher
     // TODO SignUp as student or teacher
     // TODO LogIn as student or teacher
@@ -15,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mgradedTest = findViewById(R.id.gradedTests);
+
+        mgradedTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ViewGradedTestActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/quizyou/User/ViewGradedTestActivity.java
+++ app/src/main/java/com/example/quizyou/User/ViewGradedTestActivity.java
@@ -1,16 +1,51 @@
 package com.example.quizyou.User;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.quizyou.R;
+import com.example.quizyou.Test.GradedTest;
+
+import java.util.ArrayList;
 
 public class ViewGradedTestActivity extends AppCompatActivity {
-
+    private TextView mtestName;
+    private TextView mtestScore;
+    private Button mBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_graded_test);
+
+        mtestName = findViewById(R.id.testName);
+        mtestScore = findViewById(R.id.testScore);
+        mBack = findViewById(R.id.viewGradeTestBack);
+
+        Student obj = new Student("Larry Zhi", "larryzhi@gmail.com", "larryzhi");
+
+        ArrayList<GradedTest> tests = obj.getReports();
+        String str = "";
+        for(int i = 0; i < tests.size(); i++){
+            str += tests.get(i);
+
+        }
+        mtestName.setText(str);
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StudentActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+
     }
 }
