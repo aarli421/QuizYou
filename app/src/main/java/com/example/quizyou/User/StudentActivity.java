51cd967da4f71ab9fac3f965afe093ee19e92e41
Zheng Andrew <andrewzheng04@gmail.com>
2020-04-13 21:10:21 -0700

    did dialog for class code

--- app/src/main/java/com/example/quizyou/User/StudentActivity.java
+++ app/src/main/java/com/example/quizyou/User/StudentActivity.java
@@ -1,11 +1,16 @@
 package com.example.quizyou.User;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-
+import android.widget.EditText;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialog;
+import androidx.appcompat.view.ContextThemeWrapper;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.quizyou.MainActivity;
@@ -13,12 +18,13 @@ import com.example.quizyou.R;
 import com.example.quizyou.Test.GradeTestActivity;
 import com.example.quizyou.Test.MakeTestActivity;
 import com.example.quizyou.Test.TestActivity;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentActivity extends Activity {
 
     private Button mLogout, mJoinClass, mMyReports, mTakeTest;
 
@@ -52,13 +58,27 @@ public class StudentActivity extends AppCompatActivity {
         mJoinClass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), GradeTestActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                finish();
-                return;
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(new ContextThemeWrapper(StudentActivity.this, R.style.dialog));
+                View mView = getLayoutInflater().inflate(R.layout.activity_join_class, null);
+
+                EditText mResponse = (EditText)  mView.findViewById(R.id.editText3);
+                Button mSubmit = (Button) mView.findViewById(R.id.submit);
+                Button mbackDialog = (Button) mView.findViewById(R.id.backDialog);
+
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                mbackDialog.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
             }
+
         });
+
 
         mMyReports.setOnClickListener(new View.OnClickListener() {
             @Override
