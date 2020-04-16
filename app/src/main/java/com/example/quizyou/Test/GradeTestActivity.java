679f63542eda7b41e1cd2976e91296eebf4931d0
larryz1230 <test@SZHI-US-LE01.am.trimblecorp.net>
2020-04-16 16:04:05 -0700

    added listview in grade test activity

--- app/src/main/java/com/example/quizyou/Test/GradeTestActivity.java
+++ app/src/main/java/com/example/quizyou/Test/GradeTestActivity.java
@@ -11,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,6 +21,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.quizyou.MainActivity;
 import com.example.quizyou.R;
 import com.example.quizyou.Test.Question.Question;
+import com.example.quizyou.Test.Question.TestResultAdapter;
 import com.example.quizyou.User.Student;
 import com.example.quizyou.User.Teacher;
 import com.example.quizyou.User.TeacherActivity;
@@ -30,12 +32,13 @@ public class GradeTestActivity extends AppCompatActivity {
 
     // TODO Display wrong errors
 
-    Spinner mSpinner;
+//    Spinner mSpinner;
     Button mSubmit, mBack, mGradeSelected;
     TestResult result = null;
 
     private ArrayList<EditText> editTexts = new ArrayList<>();
     private final static String TAG = "GradeTestActivity";
+    private ListView mListView;
 
 
     @Override
@@ -43,9 +46,10 @@ public class GradeTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_test);
 
-        mSpinner = findViewById(R.id.spinnerTests);
+//        mSpinner = findViewById(R.id.spinnerTests);
         mBack = findViewById(R.id.back_button);
         mGradeSelected = findViewById(R.id.gradeSelectedButton);
+        mListView = findViewById(R.id.grade_list_view);
 
         Log.d(TAG, ((Teacher) MainActivity.u).getResults().toString());
 
@@ -65,110 +69,120 @@ public class GradeTestActivity extends AppCompatActivity {
             list.add(t.getAnswerer().getName() + "'s " + t.getTest().getName());
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        mSpinner.setAdapter(adapter);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_spinner_item, list);
+//        mSpinner.setAdapter(adapter);
 
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                LinearLayout layout = findViewById(R.id.linearLayout);
-                layout.removeAllViews();
+//        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                LinearLayout layout = findViewById(R.id.linearLayout);
+//                layout.removeAllViews();
+//
+//                editTexts.clear();
+//
+//                String name = parent.getItemAtPosition(position).toString();
+//
+//                for (TestResult t : ((Teacher) MainActivity.u).getResults()) {
+//                    if ((t.getAnswerer().getName() + "'s " + t.getTest().getName()).equals(name)) {
+//                        result = t;
+//                    }
+//                }
+//
+//                TextView text = new TextView(getApplicationContext());
+//                text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//                Typeface face = Typeface.create("sans-serif-light", Typeface.NORMAL);
+//                text.setTypeface(face);
+//                text.setTextSize(36);
+//                text.setText("Student exited the app " + result.getExitedApp() + " times during the test");
+//                layout.addView(text);
+//
+//                int totalScore = 0;
+//                if (result != null) {
+//                    for (int i = 0; i < result.getQuestions().size(); i++) {
+//
+//                        text = new TextView(getApplicationContext());
+//                        text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//                        text.setTypeface(face);
+//                        text.setTextSize(36);
+//                        text.setText(result.getQuestions().get(i).getPrompt() + "\nYour Answer: " + result.getQuestions().get(i).getAnswer() +
+//                                "\nStudent's Answer: " + result.getAnswers().get(i));
+//                        layout.addView(text);
+//
+//                        EditText editText = new EditText(getApplicationContext());
+//                        editText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//                        editText.setHint("Enter points out of total points " + result.getQuestions().get(i).getPoints());
+//
+//                        totalScore += result.getQuestions().get(i).getPoints();
+//
+//                        editTexts.add(editText);
+//                        layout.addView(editText);
+//                    }
+//
+//                    final EditText notes = new EditText(getApplicationContext());
+//                    notes.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+//                    notes.setHint("Enter some notes...");
+//                    layout.addView(notes);
+//
+//                    mSubmit = new Button(getApplicationContext());
+//                    mSubmit.setText("Submit");
+//                    layout.addView(mSubmit);
+//
+//                    final int finalTotalScore = totalScore;
+//                    mSubmit.setOnClickListener(new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View v) {
+//                            if (result != null) {
+//                                MainActivity.load();
+//
+//                                Log.d(TAG, editTexts.toString());
+//
+//                                int score = 0;
+//                                for (int i = 0; i < editTexts.size(); i++) {
+//                                    score += Integer.parseInt(editTexts.get(i).getText().toString());
+//                                }
+//
+//                                final String note = notes.getText().toString();
+//
+//                                ((Teacher) MainActivity.u).removeTestResults(result);
+//                                ((Teacher) MainActivity.u).addGradedTests(new GradedTest(result.getTest(), score, finalTotalScore, note, result.getAnswerer().getID()));
+//                                result.getAnswerer().addReport(new GradedTest(result.getTest(), score, finalTotalScore, note, result.getAnswerer().getID()));
+//                                Log.d(TAG, "Score: " + score + " Final Score: " + finalTotalScore + " Note: " + note);
+//
+//                                MainActivity.mDb.collection("Students")
+//                                        .document(Long.toString(result.getAnswerer().getID()))
+//                                        .update("reports", result.getAnswerer().getReports());
+//
+//                                MainActivity.mDb.collection("Teachers")
+//                                        .document(Long.toString(((Teacher) MainActivity.u).getID()))
+//                                        .update("results", ((Teacher) MainActivity.u).getResults(),
+//                                                "gradedTests", ((Teacher) MainActivity.u).getGradedTests());
+//
+//                                //MainActivity.save();
+//                                //MainActivity.save(result.getAnswerer());
+//
+//                                startActivity(new Intent(getApplicationContext(), TeacherActivity.class));
+//                                finish();
+//                                return;
+//                            }
+//                        }
+//                    });
+//                }
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
-                editTexts.clear();
+        ArrayList<TestResult> testResults = new ArrayList<>();
 
-                String name = parent.getItemAtPosition(position).toString();
 
-                for (TestResult t : ((Teacher) MainActivity.u).getResults()) {
-                    if ((t.getAnswerer().getName() + "'s " + t.getTest().getName()).equals(name)) {
-                        result = t;
-                    }
-                }
+        for(int i = 0; i<((Teacher)MainActivity.u).getResults().size(); i++){
+            testResults.add(((Teacher)MainActivity.u).getResults().get(i));
+        }
 
-                TextView text = new TextView(getApplicationContext());
-                text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                Typeface face = Typeface.create("sans-serif-light", Typeface.NORMAL);
-                text.setTypeface(face);
-                text.setTextSize(36);
-                text.setText("Student exited the app " + result.getExitedApp() + " times during the test");
-                layout.addView(text);
-
-                int totalScore = 0;
-                if (result != null) {
-                    for (int i = 0; i < result.getQuestions().size(); i++) {
-
-                        text = new TextView(getApplicationContext());
-                        text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                        text.setTypeface(face);
-                        text.setTextSize(36);
-                        text.setText(result.getQuestions().get(i).getPrompt() + "\nYour Answer: " + result.getQuestions().get(i).getAnswer() +
-                                "\nStudent's Answer: " + result.getAnswers().get(i));
-                        layout.addView(text);
-
-                        EditText editText = new EditText(getApplicationContext());
-                        editText.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                        editText.setHint("Enter points out of total points " + result.getQuestions().get(i).getPoints());
-
-                        totalScore += result.getQuestions().get(i).getPoints();
-
-                        editTexts.add(editText);
-                        layout.addView(editText);
-                    }
-
-                    final EditText notes = new EditText(getApplicationContext());
-                    notes.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                    notes.setHint("Enter some notes...");
-                    layout.addView(notes);
-
-                    mSubmit = new Button(getApplicationContext());
-                    mSubmit.setText("Submit");
-                    layout.addView(mSubmit);
-
-                    final int finalTotalScore = totalScore;
-                    mSubmit.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if (result != null) {
-                                MainActivity.load();
-
-                                Log.d(TAG, editTexts.toString());
-
-                                int score = 0;
-                                for (int i = 0; i < editTexts.size(); i++) {
-                                    score += Integer.parseInt(editTexts.get(i).getText().toString());
-                                }
-
-                                final String note = notes.getText().toString();
-
-                                ((Teacher) MainActivity.u).removeTestResults(result);
-                                ((Teacher) MainActivity.u).addGradedTests(new GradedTest(result.getTest(), score, finalTotalScore, note, result.getAnswerer().getID()));
-                                result.getAnswerer().addReport(new GradedTest(result.getTest(), score, finalTotalScore, note, result.getAnswerer().getID()));
-                                Log.d(TAG, "Score: " + score + " Final Score: " + finalTotalScore + " Note: " + note);
-
-                                MainActivity.mDb.collection("Students")
-                                        .document(Long.toString(result.getAnswerer().getID()))
-                                        .update("reports", result.getAnswerer().getReports());
-
-                                MainActivity.mDb.collection("Teachers")
-                                        .document(Long.toString(((Teacher) MainActivity.u).getID()))
-                                        .update("results", ((Teacher) MainActivity.u).getResults(),
-                                                "gradedTests", ((Teacher) MainActivity.u).getGradedTests());
-
-                                //MainActivity.save();
-                                //MainActivity.save(result.getAnswerer());
-
-                                startActivity(new Intent(getApplicationContext(), TeacherActivity.class));
-                                finish();
-                                return;
-                            }
-                        }
-                    });
-                }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+        TestResultAdapter adapter1 = new TestResultAdapter(this, R.layout.adapter_grade_view_layout, testResults);
+        mListView.setAdapter(adapter1);
     }
 }
