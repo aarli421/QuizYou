5e00fa372096661629ee79f828256f51c9b445eb
larryz1230 <test@SZHI-US-LE01.am.trimblecorp.net>
2020-04-16 20:55:16 -0700

    added animations

--- app/src/main/java/com/example/quizyou/SignUpActivity.java
+++ app/src/main/java/com/example/quizyou/SignUpActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
